cd1ef62613d65233d67e3c9ca319c2e925743596
James McELwain <jmcelwain@gmail.com>
2017-12-31 14:41:40 -0600

    don't use separate file for actions, error handling in route, kiss

--- src/clj/vielheit/routes/register.clj
+++ src/clj/vielheit/routes/register.clj
@@ -1,10 +1,28 @@
 (ns vielheit.routes.register
   (:require
+   [clojure.tools.logging :as log]
    [compojure.core :refer [defroutes POST]]
+   [vielheit.db.core :as db]
+   [vielheit.services.auth :as auth]
    [ring.util.http-response :as response]
    [vielheit.util.error :refer [unwrap*]]
    [vielheit.services.register :as register]))
 
+(defn- error-unique? [e]
+  (clojure.string/includes? (-> e .getCause .getMessage) "uq_email"))
+
+(defn- handle-register-error [e user]
+  (case
+      {}(error-unique? e)
+    (do
+      (log/error (str "email exists " (:email user)) e)
+      (response/bad-request {:error "email exists"
+                             :field :email
+                             :value (:email user)}))))
+
 (defroutes register-routes
   (POST "/register" {user :params}
-        (unwrap* (register/register user))))
+        (try
+          (response/ok (db/create-user! (auth/encrypt-password user)))
+          (catch Exception e (handle-register-error e user)))))
+
--- src/clj/vielheit/services/register.clj
+++ src/clj/vielheit/services/register.clj
@@ -1,6 +1,7 @@
 (ns vielheit.services.register
   (:require
    [clojure.tools.logging :as log]
+   
    [vielheit.db.core :as db]
    [vielheit.services.auth :as auth]
    [vielheit.util.error :as error]
@@ -10,12 +11,4 @@
   (clojure.string/includes? (-> e .getCause .getMessage) "uq_email"))
 
 (defn register [user]
-  (try
-    (db/create-user! (auth/encrypt-password user))
-    (catch Exception e
-      (case
-          (error-unique? e) (do
-                              (log/error (str "email exists " (:email user)) e)
-                              (error/error (response/bad-request {:error "email exists"
-                                                                  :field :email
-                                                                  :value (:email user)})))))))
+ )
