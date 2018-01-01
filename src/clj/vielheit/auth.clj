3b3f9259808d71b1a02f5c64ea1935cedb3330e3
James McELwain <jmcelwain@gmail.com>
2018-01-01 17:53:35 -0600

    function order

--- src/clj/vielheit/auth.clj
+++ src/clj/vielheit/auth.clj
@@ -3,15 +3,15 @@
    [vielheit.db.core :as db]
    [buddy.hashers :as hashers]))
 
+(defn hash-password [password]
+  (hashers/derive password))
+
 (defn password-matches? [email password]
   (if-let [user (db/get-user-by-email {:email email})]
     (do
       (println (hash-password password) (:pass user))
       (hashers/check password (:pass user)))
     false))
-
-(defn hash-password [password]
-  (hashers/derive password))
 
 (defn encrypt-password [user]
   (assoc user :pass (hash-password (:pass user))))
